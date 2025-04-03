3ba6fd391b78e18d119b6cd3254af8d66c74ca0a
PMarinov1994 <plamen_944@outlook.com>
2025-04-04 01:34:50 +0300

    Update keys

--- keyboards/lily58/keymaps/pmarinov1994/keymap.c
+++ keyboards/lily58/keymaps/pmarinov1994/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_L1] = LAYOUT(
   _______,       _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
   KC_F1,         KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  SFT_T(KC_ESC), GUI_T(KC_EXLM), ALT_T(KC_AT),   SFT_T(KC_HASH), CTL_T(KC_DLR),  KC_PERC,                   KC_CIRC, CTL_T(KC_AMPR), SFT_T(KC_ASTR), ALT_T(KC_LPRN), GUI_T(KC_RPRN), KC_TILD,
+  SFT_T(KC_ESC), KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
   _______,       KC_QUES, _______, KC_TILD, KC_SLSH, KC_EQL,  _______, _______, KC_BSLS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                                    _______, _______, XXXXXXX, _______, _______, KC_BSPC, _______, _______
 ),
