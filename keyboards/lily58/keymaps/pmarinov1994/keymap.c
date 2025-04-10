dd46a44688f6c05172adb274f2ed24921c7e6e43
PMarinov1994 <plamen_944@outlook.com>
2025-04-10 23:04:43 +0300

    Remove Alt & WinKey from homerow mods. Use last keys on the bottom row

--- keyboards/lily58/keymaps/pmarinov1994/keymap.c
+++ keyboards/lily58/keymaps/pmarinov1994/keymap.c
@@ -41,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2, KC_3,    KC_4,    KC_5,                             KC_6,       KC_7,         KC_8,        KC_9,         KC_0,           KC_EQL,
-  KC_TAB,   KC_Q,   KC_W, KC_E,    KC_R,    KC_T,                             KC_Y,       KC_U,         KC_I,        KC_O,         KC_P,           KC_MINS,
-  KC_ESC,   KC_A,   KC_S, SFT_T(KC_D), CTL_T(KC_F), KC_G,                     KC_H,       CTL_T(KC_J),  SFT_T(KC_K), ALT_T(KC_L),  GUI_T(KC_SCLN), KC_QUOT,
+  KC_GRV,   KC_1,   KC_2, KC_3,    KC_4,    KC_5,                             KC_6,       KC_7,         KC_8,        KC_9,  KC_0,    KC_EQL,
+  KC_TAB,   KC_Q,   KC_W, KC_E,    KC_R,    KC_T,                             KC_Y,       KC_U,         KC_I,        KC_O,  KC_P,    KC_MINS,
+  KC_ESC,   KC_A,   KC_S, SFT_T(KC_D), CTL_T(KC_F), KC_G,                     KC_H,       CTL_T(KC_J),  SFT_T(KC_K), KC_L,  KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,              KC_RBRC,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RCTL,
-                       KC_LGUI, KC_LALT, MO(_L1), KC_SPC,                    KC_ENT, MO(_L2), KC_BSPC, KC_BSPC
+                       KC_LGUI, KC_LALT, MO(_L1), KC_SPC,                    KC_ENT, MO(_L2), KC_RALT, KC_RGUI
 ),
 
 
@@ -98,11 +98,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_L2] = LAYOUT(
-  KC_PSCR, _______,    _______,    _______,        _______,       _______,                         _______, _______,        _______,      _______,        _______, KC_TG_LAYER_GAMING,
-  KC_TAB,  KC_1,       KC_2,       KC_3,           KC_4,          KC_5,                            KC_6,    KC_7,           KC_8,         KC_9,           KC_0,    XXXXXXX,
-  _______, LCTL(KC_C), LCTL(KC_V), SFT_T(KC_HOME), CTL_T(KC_END), KC_DEL,                          KC_LEFT, CTL_T(KC_DOWN), SFT_T(KC_UP), ALT_T(KC_RGHT), KC_RGUI, XXXXXXX,
-  _______, RCS(KC_C),  RCS(KC_V),  KC_PGDN,        KC_PGUP,       XXXXXXX, LCTL(KC_Z),    LCTL(KC_Y), KC_PLUS, KC_MINS,        KC_COMMA,     KC_DOT,         KC_EQL,  KC_SLASH,
-                                                   _______, _______, KC_BSPC, _______,    _______,    XXXXXXX, _______, KC_RGUI
+  KC_PSCR, _______,    _______,    _______,        _______,       _______,                     _______, _______,        _______,      _______, _______, KC_TG_LAYER_GAMING,
+  KC_TAB,  KC_1,       KC_2,       KC_3,           KC_4,          KC_5,                        KC_6,    KC_7,           KC_8,         KC_9,    KC_0,    XXXXXXX,
+  _______, LCTL(KC_C), LCTL(KC_V), SFT_T(KC_HOME), CTL_T(KC_END), KC_DEL,                      KC_LEFT, CTL_T(KC_DOWN), SFT_T(KC_UP), KC_RGHT, KC_RGUI, XXXXXXX,
+  _______, RCS(KC_C),  RCS(KC_V),  KC_PGDN,        KC_PGUP,       XXXXXXX, LCTL(KC_Z),    LCTL(KC_Y), KC_PLUS, KC_MINS,        KC_COMMA,     KC_DOT,    KC_EQL,  KC_SLASH,
+                                                   _______, _______, KC_BSPC, _______,    _______,    XXXXXXX, _______, _______
 ),
 
 
