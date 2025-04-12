4d3197e64254f51ef80116038be5ca63b81b5ba4
PMarinov1994 <plamen_944@outlook.com>
2025-04-12 11:49:52 +0300

    Revert back ALT and GUI modifiers on homerow. Add Application context on L1.

--- keyboards/lily58/keymaps/pmarinov1994/keymap.c
+++ keyboards/lily58/keymaps/pmarinov1994/keymap.c
@@ -41,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2, KC_3,    KC_4,    KC_5,                             KC_6,       KC_7,         KC_8,        KC_9,  KC_0,    KC_EQL,
-  KC_TAB,   KC_Q,   KC_W, KC_E,    KC_R,    KC_T,                             KC_Y,       KC_U,         KC_I,        KC_O,  KC_P,    KC_MINS,
-  KC_ESC,   KC_A,   KC_S, SFT_T(KC_D), CTL_T(KC_F), KC_G,                     KC_H,       CTL_T(KC_J),  SFT_T(KC_K), KC_L,  KC_SCLN, KC_QUOT,
+  KC_GRV,   KC_1,   KC_2, KC_3,    KC_4,    KC_5,                             KC_6,       KC_7,         KC_8,        KC_9,         KC_0,    KC_EQL,
+  KC_TAB,   KC_Q,   KC_W, KC_E,    KC_R,    KC_T,                             KC_Y,       KC_U,         KC_I,        KC_O,         KC_P,    KC_MINS,
+  KC_ESC,   KC_A,   KC_S, SFT_T(KC_D), CTL_T(KC_F), KC_G,                     KC_H,       CTL_T(KC_J),  SFT_T(KC_K), ALT_T(KC_L),  GUI_T(KC_SCLN), KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,              KC_RBRC,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RCTL,
-                       KC_LGUI, KC_LALT, MO(_L1), KC_SPC,                    KC_ENT, MO(_L2), KC_RALT, KC_RGUI
+                       KC_LGUI, KC_LALT, MO(_L1), KC_SPC,                    KC_ENT, MO(_L2), KC_BSPC, KC_RGUI
 ),
 
 
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,         KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   SFT_T(KC_ESC), KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
   _______,       KC_QUES, _______, KC_TILD, KC_SLSH, KC_EQL,  _______, _______, KC_BSLS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                                   _______, _______, XXXXXXX, _______, _______, KC_BSPC, _______, _______
+                                   _______, _______, XXXXXXX, _______, _______, KC_BSPC, KC_APP,  _______
 ),
 
 
